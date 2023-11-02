d4122d5de1c35302556bae473844815016b1f7bb
nicolas <nicolsgonza765@gmail.com>
2023-11-02 13:38:15 -0300

    first commit

---
+++ src/test/java/chacotek/esqueleto/ecommerce/ApplicationTests.java
@@ -0,0 +1,13 @@
+package chacotek.esqueleto.ecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
