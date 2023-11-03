61e0be15727f5eeae4ed03cacddfd3e5d70baf2a
nicolas <nicolsgonza765@gmail.com>
2023-11-02 23:54:12 -0300

    version 1.3

--- src/main/java/chacotek/esqueleto/ecommerce/Application.java
+++ src/main/java/chacotek/esqueleto/ecommerce/Application.java
@@ -1,7 +1,11 @@
 package chacotek.esqueleto.ecommerce;
 
+import chacotek.esqueleto.ecommerce.models.Client;
+import chacotek.esqueleto.ecommerce.repositories.ClientRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class Application {
@@ -10,4 +14,11 @@ public class Application {
 		SpringApplication.run(Application.class, args);
 	}
 
+	@Bean
+	public CommandLineRunner initData(ClientRepository clientRepository){
+		return (args) ->{
+			Client client = new Client("nombre", "direccion", "12345678", "1234567890", "nombre@correo.com" );
+			clientRepository.save(client);
+		};
+	}
 }
---
+++ src/main/java/chacotek/esqueleto/ecommerce/controllers/ClientController.java
@@ -0,0 +1,17 @@
+package chacotek.esqueleto.ecommerce.controllers;
+
+import chacotek.esqueleto.ecommerce.repositories.ClientRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class ClientController {
+
+    @Autowired
+    private ClientRepository clientRepository;
+
+}
