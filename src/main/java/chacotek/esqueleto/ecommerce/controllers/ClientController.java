e28d3a1550b353e36b4a18c9b1d746b35fd782a1
nicolas <nicolsgonza765@gmail.com>
2023-11-05 12:22:07 -0300

    version 3.2

--- src/main/java/chacotek/esqueleto/ecommerce/controllers/ClientController.java
+++ src/main/java/chacotek/esqueleto/ecommerce/controllers/ClientController.java
@@ -1,12 +1,13 @@
 package chacotek.esqueleto.ecommerce.controllers;
 
 import chacotek.esqueleto.ecommerce.dtos.ClientDTO;
+import chacotek.esqueleto.ecommerce.models.Client;
 import chacotek.esqueleto.ecommerce.repositories.ClientRepository;
+import org.apache.coyote.Response;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.stream.Collectors;
@@ -21,6 +22,30 @@ public class ClientController {
     @GetMapping("/clients")
     public List<ClientDTO>getClients(){
         return clientRepository.findAll().stream().map(ClientDTO::new).collect(Collectors.toList());
+    }
+
+    @PostMapping("/created/client")
+    public ResponseEntity<Object>createdClient(@RequestBody ClientDTO clientDTO){
+        if (clientDTO.getEmail().isBlank()){
+            return new ResponseEntity<>("The email field is empty or incorrect", HttpStatus.FORBIDDEN);
+        }
+        if (clientDTO.getPassword().isBlank()){
+            return new ResponseEntity<>("The password field is empty or incorrect", HttpStatus.FORBIDDEN);
+        }
+        if (clientDTO.getName().isBlank()){
+            return new ResponseEntity<>("The name field is empty or incorrect", HttpStatus.FORBIDDEN);
+        }
+        if (clientDTO.getDirection().isBlank()){
+            return new ResponseEntity<>("The direction field is empty or incorrect", HttpStatus.FORBIDDEN);
+        }
+        if (clientDTO.getPhoneNumber().isBlank() && clientDTO.getPhoneNumber().isEmpty()){
+            return new ResponseEntity<>("The phone number field is empty or incorrect", HttpStatus.FORBIDDEN);
+        }
+
+        Client client = new Client(clientDTO.getName(), clientDTO.getDirection(), clientDTO.getPassword(), clientDTO.getPhoneNumber(), clientDTO.getEmail());
+        clientRepository.save(client);
+
+        return new ResponseEntity<>("the user has been created successfully", HttpStatus.CREATED);
     }
 
 }
--- src/main/java/chacotek/esqueleto/ecommerce/dtos/ClientDTO.java
+++ src/main/java/chacotek/esqueleto/ecommerce/dtos/ClientDTO.java
@@ -2,10 +2,26 @@ package chacotek.esqueleto.ecommerce.dtos;
 
 import chacotek.esqueleto.ecommerce.models.Client;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+
 public class ClientDTO {
 
     private Long id;
-    private String name, direction, password, phoneNumber;
+    @NotBlank(message = "El nombre no puede estar vacío")
+    private String name;
+
+    @NotBlank(message = "La dirección no puede estar vacía")
+    private String direction;
+
+    @NotBlank(message = "La contraseña no puede estar vacía")
+    private String password;
+
+    @NotBlank(message = "El número de teléfono no puede estar vacío")
+    private String phoneNumber;
+
+    @NotBlank(message = "El correo electrónico no puede estar vacío")
+    @Email(message = "Debe ser una dirección de correo electrónico válida")
     private String email;
 
     public ClientDTO() {
