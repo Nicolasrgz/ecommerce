548b687d5d86cc09318b78ceab22a0996e1985c4
nicolas <nicolsgonza765@gmail.com>
2023-11-06 15:14:49 -0300

    Version 4.2

--- src/main/java/chacotek/esqueleto/ecommerce/controllers/ProductController.java
+++ src/main/java/chacotek/esqueleto/ecommerce/controllers/ProductController.java
@@ -1,15 +1,19 @@
 package chacotek.esqueleto.ecommerce.controllers;
 
 import chacotek.esqueleto.ecommerce.dtos.ProductDTO;
+import chacotek.esqueleto.ecommerce.models.Client;
 import chacotek.esqueleto.ecommerce.models.Product;
+import chacotek.esqueleto.ecommerce.repositories.ClientRepository;
 import chacotek.esqueleto.ecommerce.repositories.ProductRepository;
 import org.apache.coyote.Response;
+import org.apache.tomcat.util.net.openssl.ciphers.Authentication;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 @RestController
@@ -18,6 +22,8 @@ public class ProductController {
 
     @Autowired
     private ProductRepository productRepository;
+    @Autowired
+    private ClientRepository clientRepository;
 
     @GetMapping("/products")
     public List<ProductDTO>getProducts(){
@@ -53,4 +59,41 @@ public class ProductController {
             return new ResponseEntity<>("the creation of the product has not been possible", HttpStatus.CONFLICT);
         }
     }
+
+    @PatchMapping("/product/{id}/deactivate")
+    public ResponseEntity<Object> deactivateProduct(@PathVariable Long id, Authentication authentication) {
+        Client admin = clientRepository.findByEmail(authentication.name());
+
+        if (admin == null) {
+            return new ResponseEntity<>("only an administrator can use this servlet", HttpStatus.BAD_REQUEST);
+        }
+
+        return productRepository.findById(id)
+                .map(product -> {
+                    product.setActive(false);
+                    productRepository.save(product);
+                    return ResponseEntity.ok().build();
+                })
+                .orElseGet(() -> ResponseEntity.notFound().build());
+    }
+
+    @PatchMapping("/product/{id}/activate")
+    public ResponseEntity<Object> activateProduct(@PathVariable Long id, Authentication authentication) {
+        Client admin = clientRepository.findByEmail(authentication.name());
+
+        if (admin == null) {
+            return new ResponseEntity<>("only an administrator can use this servlet", HttpStatus.BAD_REQUEST);
+        }
+
+        return productRepository.findById(id)
+                .map(product -> {
+                    product.setActive(true);
+                    productRepository.save(product);
+                    return ResponseEntity.ok().build();
+                })
+                .orElseGet(() -> ResponseEntity.notFound().build());
+    }
+
+
+
 }
