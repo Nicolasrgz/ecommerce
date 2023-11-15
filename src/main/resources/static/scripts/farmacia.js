4e12463a1f8a1d5f87e81777a7d814ef4c1fc048
nicolas <nicolsgonza765@gmail.com>
2023-11-15 11:36:54 -0300

    version 8.1

--- src/main/resources/static/scripts/farmacia.js
+++ src/main/resources/static/scripts/farmacia.js
@@ -3,54 +3,31 @@ const { createApp } = Vue;
 const app = createApp({
   data() {
     return {
-      allEvents: [],
-      farmacia: [],
-      busqueda: "",
       seleccionadas: [],
-      filtroTitulo: [],
+      products: [],
     };
   },
 
   created() {
-    this.seleccionadas = this.getLocalStorage() ?? [];
-    fetch("https://mindhub-xj03.onrender.com/api/petshop")
-      .then((res) => res.json())
-      .then((data) => {
-        this.allEvents = data;
-        this.farmacia = this.allEvents.filter(
-          (fil) => fil.categoria == "farmacia"
-        );
-      })
-      .catch((error) => console.error(error));
+    // this.seleccionadas = this.getLocalStorage() ?? [];
+    this.productos();
   },
 
   methods: {
-    filtrarPorTitulo() {
-      return this.farmacia.filter((e) =>
-        e.producto.toLowerCase().includes(this.busqueda.toLowerCase())
-      );
-    },
-    toggleSeleccion(_id) {
-      if (this.seleccionadas.find((e) => e._id == _id)) {
-        console.log("ya");
-        this.seleccionadas = this.seleccionadas.filter((e) => e._id != _id);
-      } else {
-        const aux = this.allEvents.find((e) => e._id == _id);
-        this.seleccionadas.push(aux);
-      }
-      const json = JSON.stringify(this.seleccionadas);
-      localStorage.setItem("compras", json);
-    },
-    getLocalStorage() {
-      return JSON.parse(localStorage.getItem("compras"));
+    productos(){
+      axios.get("http://localhost:8080/api/products")
+      .then(response => {
+        this.products = response.data
+        console.log("los productos son:", this.products)
+      })
+      .catch(error =>{
+        console.error("el llamado a la api fallo", error)
+      })
     },
   },
+
   computed: {
-    filtrarPorTitulo() {
-      this.productosFiltrados = this.farmacia.filter((e) =>
-        e.producto.toLowerCase().includes(this.busqueda.toLowerCase())
-      );
-    },
+
   },
 });
 
